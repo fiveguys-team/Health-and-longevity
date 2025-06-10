b705810d444a34f3400fecd6bc5ba814c003233e
Cmk1031 <maple1031@naver.com>
2025-06-10 10:52:36 +0900

    feat: 라이브 시작 시 라이브 정보 저장하는 LiveInfoVO

---
+++ livecommerce-server/src/main/java/com/example/livecommerce_server/live/vo/LiveInfoVO.java
@@ -0,0 +1,24 @@
+package com.example.livecommerce_server.live.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LiveInfoVO {
+	private String live_id;
+	private String vendor_id;
+	private String session_id;
+	private String title;
+	private String start_time;
+	//private String end_time;
+	//private String thumbnail;
+	private String status;
+	private String announcement;
+}
