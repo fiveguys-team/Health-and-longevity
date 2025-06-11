b0153ca1c4171fe3c1390ada342dabfb214c9831
Cmk1031 <maple1031@naver.com>
2025-06-11 11:04:35 +0900

    feat: 라이브 정보, 상품 저장 기능 구현

--- livecommerce-server/src/main/java/com/example/livecommerce_server/live/controller/LiveController.java
+++ livecommerce-server/src/main/java/com/example/livecommerce_server/live/controller/LiveController.java
@@ -1,5 +1,12 @@
 package com.example.livecommerce_server.live.controller;
 
+import com.example.livecommerce_server.live.dto.LiveChatDTO;
+import com.example.livecommerce_server.live.dto.LiveDTO;
+import com.example.livecommerce_server.live.dto.LiveProductDTO;
+import com.example.livecommerce_server.live.dto.ProductInfo;
+import com.example.livecommerce_server.live.service.LiveService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 import io.openvidu.java.client.Connection;
 import io.openvidu.java.client.ConnectionProperties;
 import io.openvidu.java.client.OpenVidu;
@@ -10,12 +17,13 @@ import io.openvidu.java.client.SessionProperties;
 import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +32,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,9 +48,14 @@ import org.springframework.web.filter.CorsFilter;
 		methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
 				RequestMethod.DELETE, RequestMethod.OPTIONS}
 )
-@RestController
+
 @Slf4j
+@RestController
+@RequiredArgsConstructor
 public class LiveController {
+	private final ObjectMapper mapper = new ObjectMapper();
+
+	private final LiveService liveService;
 
 	@Value("${OPENVIDU_URL}")
 	private String OPENVIDU_URL;
@@ -52,7 +66,7 @@ public class LiveController {
 	private OpenVidu openvidu;
 
 	// 활성 세션 정보를 저장하는 맵
-	private final ConcurrentHashMap<String, SessionInfo> activeSessions = new ConcurrentHashMap<>();
+	private final ConcurrentHashMap<String, LiveDTO> activeSessions = new ConcurrentHashMap<>();
 
 	@PostConstruct
 	public void init() {
@@ -75,35 +89,89 @@ public class LiveController {
 	 * 현재 활성화된 모든 세션 목록을 반환
 	 */
 	@GetMapping("/api/sessions")
-	public ResponseEntity<List<SessionInfo>> getActiveSessions() {
-		List<SessionInfo> sessions = new ArrayList<>(activeSessions.values());
+	public ResponseEntity<List<LiveDTO>> getActiveSessions() {
+		List<LiveDTO> sessions = new ArrayList<>(activeSessions.values());
 		return new ResponseEntity<>(sessions, HttpStatus.OK);
 	}
 
+//	/**
+//	 * @param params The Session properties
+//	 * @return The Session ID
+//	 */
+//	@PostMapping("/api/sessions")
+//	public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)
+//			throws OpenViduJavaClientException, OpenViduHttpException {
+	//  세션 생성시 옵션(속성)을 담는 빌더 클래스 (mediaMode, customSessionId 등등)
+//		SessionProperties properties = SessionProperties.fromJson(params).build();
+	// 속성 정보를 바탕으로 session 객체 생성 / 내부에 sessionId 식별자 존재 / 클라이언트가 토큰 발급 시 사용
+//		Session session = openvidu.createSession(properties);
+//		return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
+//	}
+
 	/**
-	 * @param params 세션 속성 정보
+	 * @param
 	 * @return sessionId
 	 */
 	@PostMapping("/api/sessions")
-	public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)
-			throws OpenViduJavaClientException, OpenViduHttpException {
-		SessionProperties properties = SessionProperties.fromJson(params).build();
-		Session session = openvidu.createSession(properties);
+	public ResponseEntity<?> initializeSession(@ModelAttribute LiveDTO liveDTO) {
+		try {
 
-		// 세션 정보 저장
-		if (params != null && params.containsKey("customSessionId")) {
-			String sessionId = params.get("customSessionId").toString();
-			SessionInfo sessionInfo = new SessionInfo();
-			sessionInfo.setId(sessionId);
-			if (params.containsKey("clientData")) {
-				Map<String, Object> clientData = (Map<String, Object>) params.get("clientData");
-				sessionInfo.setTitle((String) clientData.get("title"));
-//				sessionInfo.setProductInfo((Map<String, Object>) clientData.get("productInfo"));
-			}
-			activeSessions.put(sessionId, sessionInfo);
+			// OpenVidu 세션 생성
+			Map<String, Object> params = new HashMap<>();
+			// 세션 ID
+			String sessionId = UUID.randomUUID().toString();
+			params.put("customSessionId", sessionId);
+
+			log.info("상품:"+liveDTO.getProducts());
+
+			SessionProperties properties = SessionProperties.fromJson(params).build();
+			Session session = openvidu.createSession(properties);
+
+			liveDTO.setSessionId(sessionId);
+
+			List<ProductInfo> prodList = mapper.readValue(
+					liveDTO.getProducts(),
+					new TypeReference<>() {
+					}
+			);
+
+//
+			// 썸네일 파일 처리
+//			if (liveDTO.getThumbnail() != null && !liveDTO.getThumbnail().isEmpty()) {
+//				// 실제 프로덕션에서는 파일 저장 서비스를 통해 S3 등에 저장하고 URL을 받아와야 함
+//				// 임시로 파일명만 저장
+//				String thumbnailFileName = liveDTO.getThumbnail().getOriginalFilename();
+//				sessionInfo.setThumbnailUrl(thumbnailFileName);
+//			}
+
+			//activeSessions.put(liveDTO.getCustomSessionId(), sessionInfo);
+			activeSessions.put(sessionId, liveDTO);
+
+			// 라이브 테이블 정보 저장
+			LiveChatDTO liveChatDTO = liveService.addLiveInfo(liveDTO);
+
+			List<LiveProductDTO> liveProducts = prodList.stream()
+					.map(m -> {
+						LiveProductDTO dto = new LiveProductDTO();
+						dto.setLiveId(liveChatDTO.getLiveId());
+						dto.setProductId(m.getId());                // id 필드만 꺼내서 productId에
+						dto.setDiscountRate(liveDTO.getDiscountRate());
+						return dto;
+					})
+					.toList();
+
+			// 라이브 상품 정보 저장
+			liveService.addLiveProduct(liveProducts);
+
+			log.info("sessionId" + session.getSessionId());
+			log.info("라이브1111111111" + liveChatDTO.getLiveId());
+			log.info("sessionId1111111111111111122222222" + liveChatDTO.getSessionId());
+			return ResponseEntity.ok(liveChatDTO);
+		} catch (Exception e) {
+			log.error("Failed to initialize session", e);
+			return new ResponseEntity<>("Failed to initialize session: " + e.getMessage(),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-
-		return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
 	}
 
 	/**
@@ -181,21 +249,3 @@ public class LiveController {
 	}
 }
 
-// 세션 정보를 담는 클래스
-@Setter
-@Getter
-class SessionInfo {
-	private String id;
-	private String title;
-	//private Map<String, Object> productInfo;
-}
-
-// 상품 정보를 담는 클래스
-@Setter
-@Getter
-@Builder
-class ProductInfo {
-	private String id;
-	private String name;
-	private Integer price;
-}
