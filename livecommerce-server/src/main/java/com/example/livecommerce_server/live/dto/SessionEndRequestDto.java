fbb1d2f5f93443f8e94b6428a97d1da913300187
Cmk1031 <maple1031@naver.com>
2025-06-11 23:04:09 +0900

    feat: 라이브 방송 종료 정보 전달 DTO

---
+++ livecommerce-server/src/main/java/com/example/livecommerce_server/live/dto/SessionEndRequestDto.java
@@ -0,0 +1,15 @@
+package com.example.livecommerce_server.live.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SessionEndRequestDto {
+	private String sessionId;
+	private String endTime;
+}
