710f81ac578d2d7aa4c1229d76f610df036f3b2a
Cmk1031 <maple1031@naver.com>
2025-06-15 00:32:26 +0900

    feat: 시청자 입정 정보 저장 mapper Test

---
+++ livecommerce-server/src/test/java/com/example/livecommerce_server/live/mapper/LiveStatisticsMapperTest.java
@@ -0,0 +1,36 @@
+package com.example.livecommerce_server.live.mapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.livecommerce_server.live.dto.LiveViewerStatsDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Slf4j
+@Transactional
+@ActiveProfiles("test")
+class LiveStatisticsMapperTest {
+
+	@Autowired(required = false)
+	private LiveStatisticsMapper liveStatisticsMapper;
+
+	@Test
+	@DisplayName("시청자 입장 정보 저장")
+	void insertViewerJoin() {
+		LiveViewerStatsDTO viewer = LiveViewerStatsDTO.builder()
+				.viewerLogId(null)
+				.liveId("cccccccc-cccc-cccc-cccc-cccccccccccc")
+				.userId("1")
+				.joinAt("2025-06-14T11:38:50.805866300Z")
+				.build();
+		liveStatisticsMapper.insertViewerJoin(viewer);
+	}
+
+
+}
