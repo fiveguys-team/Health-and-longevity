123cd399b858fa5d508c63aca5c3d07daad471cd
portsungbin <sd61224@gmail.com>
2025-06-16 17:37:17 +0900

    chore: 전역 라우터 및 설정 파일 정리

--- livecommerce-server/src/main/java/com/example/livecommerce_server/config/WebConfig.java
+++ livecommerce-server/src/main/java/com/example/livecommerce_server/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.example.livecommerce_server.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,4 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/images/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/images/")
+                .setCachePeriod(3600); // 캐싱
+    }
+
 }
