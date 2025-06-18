595f94a1c0a45d2f520f5d0246ff37ee48a8634c
Cmk1031 <maple1031@naver.com>
2025-06-18 15:55:18 +0900

    feat: 관리자 대시보드의 이번 달 주문 구현
    - 이번달 주문수
    - 저번달 주문수
    - service 구현

---
+++ livecommerce-server/src/main/java/com/example/livecommerce_server/adminDashboard/service/AdminDashboardService.java
@@ -0,0 +1,9 @@
+package com.example.livecommerce_server.adminDashboard.service;
+
+import com.example.livecommerce_server.adminDashboard.dto.MonthOrdersDTO;
+
+public interface AdminDashboardService {
+
+	MonthOrdersDTO findMonthOrders();
+
+}
---
+++ livecommerce-server/src/main/java/com/example/livecommerce_server/adminDashboard/service/AdminDashboardServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.livecommerce_server.adminDashboard.service;
+
+import com.example.livecommerce_server.adminDashboard.dto.MonthOrdersDTO;
+import com.example.livecommerce_server.adminDashboard.mapper.AdminDashboardMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AdminDashboardServiceImpl implements AdminDashboardService {
+
+	private final AdminDashboardMapper adminDashboardMapper;
+	@Override
+	public MonthOrdersDTO findMonthOrders() {
+		int monthOrders = adminDashboardMapper.selectMonthOrders();
+		int previousMonthOrders = adminDashboardMapper.selectPreviousMonthOrders();
+		log.info(String.valueOf(monthOrders));
+		log.info(String.valueOf(previousMonthOrders));
+
+		int orderRate = 0;
+		if(previousMonthOrders!=0) {
+			orderRate = (monthOrders - previousMonthOrders) / previousMonthOrders * 100;
+		}
+
+		return MonthOrdersDTO.builder()
+				.orderCount(monthOrders)
+				.orderCountChangeRate(orderRate)
+				.build();
+	}
+
+
+}
